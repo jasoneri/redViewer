dd513ea9d39854be1558b691e8bc2f7249d52e12
json <jsoneri@outlook.com>
2026-01-13 01:57:07 +0800

    upd: doc components

---
+++ docs/.vitepress/theme/MyLayout.vue
@@ -0,0 +1,16 @@
+<script setup>
+import DefaultTheme from 'vitepress/theme'
+const { Layout } = DefaultTheme
+</script>
+
+<template>
+  <Layout>
+    <template #home-hero-info-after>
+      <div class="home-demo">
+        <a class="home-demo__btn" href="https://demo.redviewer.nyc.mn" target="_blank" rel="noreferrer">
+          🎮 在线体验 Demo
+        </a>
+      </div>
+    </template>
+  </Layout>
+</template>
---
+++ docs/.vitepress/theme/index.js
@@ -0,0 +1,8 @@
+import DefaultTheme from 'vitepress/theme'
+import MyLayout from './MyLayout.vue'
+import './custom.css'
+
+export default {
+  extends: DefaultTheme,
+  Layout: MyLayout
+}
